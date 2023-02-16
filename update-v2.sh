b82e5d62f8fd2429c029cd74b3a48c3a57b76da5
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-16 11:01:54 +0800

    Update update-v2.sh

--- update-v2.sh
+++ update-v2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#Script Updater By wunuit
+#Script Updater By annelyah23
 red='\e[1;31m'
 green='\e[0;32m'
 purple='\e[0;35m'
 orange='\e[0;33m'
 NC='\e[0m'
 # // Exporting URL Host
-export Server_URL="raw.githubusercontent.com/wunuit/Multiport/main"
+export Server_URL="raw.githubusercontent.com/annelyah23/xyz/main"
 clear
 if [[ -e /usr/local/bin/xray ]]; then
      echo ""
@@ -35,11 +35,11 @@ if [[ -e /usr/local/bin/xray ]]; then
 	 wget -O ins-helium "https://${Server_URL}/ins-helium.sh" && chmod +x ins-helium
 	 wget -O menu "https://${Server_URL}/menu.sh" && chmod +x menu
 	 wget -O limit "https://${Server_URL}/limit-speed.sh" && chmod +x limit
-	 #wget -O add-tr "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/add-tr.sh" && chmod +x add-tr
-	 #wget -O add-vless "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/add-vless.sh" && chmod +x add-vless
-	 #wget -O add-ws "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/add-ws.sh" && chmod +x add-ws
-	 #wget -O user-tr "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/user-tr.sh" && chmod +x user-tr
-	 #wget -O user-vless "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 #wget -O add-tr "https://raw.githubusercontent.com/annelyah23/xyz/main/add-tr.sh" && chmod +x add-tr
+	 #wget -O add-vless "https://raw.githubusercontent.com/annelyah23/xyz/main/add-vless.sh" && chmod +x add-vless
+	 #wget -O add-ws "https://raw.githubusercontent.com/annelyah23/xyz/main/add-ws.sh" && chmod +x add-ws
+	 #wget -O user-tr "https://raw.githubusercontent.com/annelyah23/xyz/main/user-tr.sh" && chmod +x user-tr
+	 #wget -O user-vless "https://raw.githubusercontent.com/annelyah23/xyz/main/user-vless.sh" && chmod +x user-vless
 	 wget -O bbr "https://${Server_URL}/bbr.sh" && chmod +x bbr
 	 echo ""
 	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
@@ -76,11 +76,11 @@ else
 	 wget -O ins-helium "https://${Server_URL}/ins-helium.sh" && chmod +x ins-helium
 	 wget -O menu "https://${Server_URL}/menu.sh" && chmod +x menu
 	 wget -O limit "https://${Server_URL}/limit-speed.sh" && chmod +x limit
-	 #wget -O add-tr "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/add-tr.sh" && chmod +x add-tr
-	 #wget -O add-vless "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/add-vless.sh" && chmod +x add-vless
-	 #wget -O add-ws "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/add-ws.sh" && chmod +x add-ws
-	 #wget -O user-tr "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/user-tr.sh" && chmod +x user-tr
-	 #wget -O user-vless "https://raw.githubusercontent.com/wunuit/MultiportWebsocketPremium-V2/main/XRAY/user-vless.sh" && chmod +x user-vless
+	 #wget -O add-tr "https://raw.githubusercontent.com/annelyah23/xyz/main/add-tr.sh" && chmod +x add-tr
+	 #wget -O add-vless "https://raw.githubusercontent.com/annelyah23/xyz/main/add-vless.sh" && chmod +x add-vless
+	 #wget -O add-ws "https://raw.githubusercontent.com/annelyah23/xyz/main/add-ws.sh" && chmod +x add-ws
+	 #wget -O user-tr "https://raw.githubusercontent.com/annelyah23/xyz/main/user-tr.sh" && chmod +x user-tr
+	 #wget -O user-vless "https://raw.githubusercontent.com/annelyah23/xyz/main//user-vless.sh" && chmod +x user-vless
 	 wget -O bbr "https://${Server_URL}/bbr.sh" && chmod +x bbr
 	 echo ""
 	 echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
