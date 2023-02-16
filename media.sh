49938b5ac51dccf4a8cadbd938455120f1e0f074
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-16 11:12:07 +0800

    Update media.sh

--- media.sh
+++ media.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Script Updater By wunuit
+#Script Updater By annelyah23
 Font_Black="\033[30m";
 Font_Red="\033[31m";
 Font_Green="\033[32m";
@@ -12,7 +12,7 @@ Font_Suffix="\033[0m";
 
 
 clear;
-echo -e "  \033[1;37m${Font_Purple}Media Stream Unlocker Test Mod By Vinstechmy${Font_Suffix}\033[0m";
+echo -e "  \033[1;37m${Font_Purple}Media Stream Unlocker Test Mod By JsPhantom${Font_Suffix}\033[0m";
 echo -e "  \033[1;37mVersion : ${Font_SkyBlue}${shell_version}${Font_Suffix}\033[0m";
 echo -e "  \033[1;37mTime    : $(date)\033[0m"
 
