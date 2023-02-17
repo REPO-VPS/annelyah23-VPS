224719a109b2afb174dab47cfbdd2fa96c616a2d
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-17 11:14:02 +0800

    Update cek-xrt.sh

--- cek-xrt.sh
+++ cek-xrt.sh
@@ -15,7 +15,7 @@ NC='\e[0m'
 echo -n > /tmp/other.txt
 data=( `cat /usr/local/etc/xray/xtrojan.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
 echo -e "\033[0;34m╔===========================================╗\033[0m"
-echo -e "\e[0;35m  |      XRAY Trojan TCP XTLS User Login        |   \033[0m"
+echo -e "\e[0;35m         XRAY Trojan TCP XTLS User Login           \033[0m"
 echo -e "\033[0;34m╚===========================================╝\033[0m"
 for akun in "${data[@]}"
 do
