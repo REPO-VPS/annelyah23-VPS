3c08ed970eba6c6fc15eed46911cec8d16fa6bbd
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-17 10:49:32 +0800

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -90,8 +90,8 @@ clear
 domain=$(cat /root/domain)
 MYIP2=$(wget -qO- ipv4.icanhazip.com);
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
-            echo -e "\033[0;34m╔============================================╗\033[0m"
-            echo -e "\033[35;1m    | Add XRAY Trojan WS Account |   \033[0m"
+             echo -e "\033[0;34m╔============================================╗\033[0m"
+            echo -e "\033[35;1m          Add XRAY Trojan WS Account    \033[0m"
             echo -e "\033[0;34m╚============================================╝\033[0m"
 
 		read -rp "Username : " -e user
@@ -99,8 +99,8 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 
 		if [[ ${user_EXISTS} == '1' ]]; then
 clear
-		    echo -e "\033[0;34m╔============================================╗\033[0m"
-            echo -e "\033[35;1m    | Add XRAY Trojan WS Account |   \033[0m"
+	 echo -e "\033[0;34m╔============================================╗\033[0m"
+            echo -e "\033[35;1m     Add XRAY Trojan WS Account    \033[0m"
             echo -e "\033[0;34m╚============================================╝\033[0m"
 			echo ""
 			echo "A client with the specified name was already created, please choose another name."
