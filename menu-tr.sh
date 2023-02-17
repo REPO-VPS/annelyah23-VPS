1bc8f61aa6e120f45445567838a925a5dff04ac9
annelyah <119406815+annelyah23@users.noreply.github.com>
2023-02-17 10:33:46 +0800

    Update menu-tr.sh

--- menu-tr.sh
+++ menu-tr.sh
@@ -10,15 +10,15 @@ B='\033[0;36m'
 N='\e[0m'
 clear
 echo -e "\e[36m╔============================================╗\033[0m"
-echo -e " \e[0;35m            XRAY TROJAN WS MENU             \033[0m"
-echo -e "\e[36m╚============================================╝\033[0m
+echo -e "\e[0;35m             XRAY TROJAN WS MENU             \033[0m"
+echo -e "\e[36m╚============================================╝\033[0m"
 
- [\033[1;36m•1 \033[0m]  Add XRAY Trojan WS Account
- [\033[1;36m•2 \033[0m]  Add Trial XRAY Trojan WS Account
- [\033[1;36m•3 \033[0m]  Check User Login XRAY Trojan WS
- [\033[1;36m•4 \033[0m]  Delete XRAY Trojan WS Account
- [\033[1;36m•5 \033[0m]  Renew XRAY Trojan WS Account
- [\033[1;36m•6 \033[0m]  Check XRAY Trojan WS Config
+ echo -e "[\033[1;36m•1 \033[0m]  \e[1;36mAdd XRAY Trojan WS Account\033[0m"
+ echo -e "[\033[1;36m•2 \033[0m]  \e[1;36mAdd Trial XRAY Trojan WS Account\033[0m"
+ echo -e "[\033[1;36m•3 \033[0m]  \e[1;36mCheck User Login XRAY Trojan WS\033[0m"
+ echo -e "[\033[1;36m•4 \033[0m]  \e[1;36mDelete XRAY Trojan WS Account\033[0m"
+ echo -e "[\033[1;36m•5 \033[0m]  \e[1;36mRenew XRAY Trojan WS Account\033[0m"
+ echo -e "[\033[1;36m•6 \033[0m]  \e[1;36mCheck XRAY Trojan WS Config\033[0m"
 
  [\033[1;36m•0 \033[0m]  Back To Main Menu"
 echo""
